7b996dcbd68cb4ba95e8e01d139752b0d85bde77
karan283861 <190244533+karan283861@users.noreply.github.com>
2026-01-29 17:38:16 +1100

    Remove logging in UFunction hook functions

--- hook.cpp
+++ hook.cpp
@@ -20,10 +20,7 @@ void __fastcall ProcessEventHook(UObject *calling_uobject,
 								 void *unused, UFunction *calling_ufunction,
 								 void *parameters, void *result)
 {
-	indent_level++;
-	PLOG_VERBOSE << GetIndentedTabString() << std::format("{0} : {1}", calling_ufunction->GetFullName(), calling_uobject->GetFullName());
 	processevent_hooks.ExecuteHook(calling_ufunction, calling_uobject, unused, calling_ufunction, parameters, result);
-	indent_level--;
 }
 
 ProcessInternalPrototype original_processinternal{reinterpret_cast<ProcessInternalPrototype>(0)};
@@ -32,11 +29,8 @@ void __fastcall ProcessInternalHook(UObject *calling_uobject,
 									void *unused, FFrame &stack,
 									void *result)
 {
-	indent_level++;
 	auto calling_ufunction = reinterpret_cast<UFunction *>(stack.node_);
-	PLOG_VERBOSE << GetIndentedTabString() << std::format("{0} : {1}", calling_ufunction->GetFullName(), calling_uobject->GetFullName());
 	processinternal_hooks.ExecuteHook(calling_ufunction, calling_uobject, unused, stack, result);
-	indent_level--;
 }
 
 CallFunctionPrototype original_callfunction{reinterpret_cast<CallFunctionPrototype>(0)};
@@ -51,11 +45,5 @@ void __fastcall CallFunctionHook(UObject *calling_uobject,
 	}
 
 	static constexpr unsigned int kFUNC_Native{0x00000400};
-	auto is_native{calling_ufunction->iNative};
-	auto is_funcnative{calling_ufunction->FunctionFlags & kFUNC_Native};
-
-	indent_level++;
-	PLOG_VERBOSE << GetIndentedTabString() << std::format("{0} {1} : {2}", is_native ? "[NATIVE]" : (is_funcnative ? "[FUNC_Native]" : ""), calling_ufunction->GetFullName(), calling_uobject->GetFullName());
 	callfunction_hooks.ExecuteHook(calling_ufunction, calling_uobject, unused, stack, result, calling_ufunction);
-	indent_level--;
 }
